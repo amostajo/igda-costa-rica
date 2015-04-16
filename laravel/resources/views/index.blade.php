5c4f82def6d55e41eff9c66e6c583931a58a49f1
Alejandro Mostajo <amostajo@amsgames.com>
2015-04-15 18:22:44 -0600

    Visual changes after tutorial

--- laravel/resources/views/index.blade.php
+++ laravel/resources/views/index.blade.php
@@ -3,6 +3,7 @@
 @section('styles')
 	<link href="{{ asset('/include/countdown/jquery.countdown.css') }}" rel="stylesheet">
 @stop
+
 @section('scripts')
 	<script src="{{ asset('/include/countdown/jquery.plugin.min.js') }}"></script>
 	<script src="{{ asset('/include/countdown/jquery.countdown.min.js') }}"></script>
--- laravel/resources/views/master.blade.php
+++ laravel/resources/views/master.blade.php
@@ -4,15 +4,15 @@
 		<title>@yield('title') - @lang('global.igda_cr')</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link href="{{ asset('css/app.css') }}" rel="stylesheet">
-		<link href="{{ asset('css/custom.css') }}" rel="stylesheet">
 		@section('styles')
 		@show
+		<link href="{{ asset('css/custom.css') }}" rel="stylesheet">
 		@section('head')
 		@show
 	</head>
 	<body>
 		@include('html.nav')
-		@section('content-no-width')
+		@section('content-no-container')
 		@show
 		<div class="container">
 			@section('content')

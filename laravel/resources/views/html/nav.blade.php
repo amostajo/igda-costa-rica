684caaec88234442d0242b5429d3cd2c7cad6f18
Alejandro Mostajo <amostajo@amsgames.com>
2015-04-15 19:22:58 -0600

    Css custom navbar

--- laravel/resources/views/html/nav.blade.php
+++ laravel/resources/views/html/nav.blade.php
@@ -1,6 +1,6 @@
 {{-- TEMPLATE PART - NAV --}}
-<nav class="navbar navbar-default navbar-static-top">
-  <div class="container">
+<div class="container">
+  <nav class="navbar navbar-default navbar-static-top">
     <div class="container-fluid">
       <!-- Brand and toggle get grouped for better mobile display -->
       <div class="navbar-header">
@@ -15,7 +15,7 @@
 
       <!-- Collect the nav links, forms, and other content for toggling -->
       <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-        <ul class="nav navbar-nav">
+        <ul class="nav navbar-nav navbar-right">
 
 		      <li><a href="#">@lang('global.program')</a></li>
 		      <li><a href="#">@lang('global.games')</a></li>
@@ -25,5 +25,5 @@
         
       </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
-  </div>
-</nav>
+  </nav>
+</div>

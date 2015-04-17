4cbb39217d9b2a75b7d5278910a8c6afc378b478
Alejandro Mostajo <amostajo@amsgames.com>
2015-04-17 12:55:55 -0600

    Css changes

--- laravel/resources/views/index.blade.php
+++ laravel/resources/views/index.blade.php
@@ -19,13 +19,13 @@
 	<div class="hero">
 
 		<div class="row holder-logo">
-			<div class="col-md-6 col-md-offset-3">
+			<div class="col-xs-6 col-xs-offset-3">
 				<img src="{{ asset('img/GFCLogo_x555.png') }}" class="img-responsive" alt="@lang('global.igda_cr')">
 			</div>
 		</div>
 
 		<div class="row holder-countdown">
-			<div class="col-md-8 col-md-offset-2">
+			<div class="col-sm-8 col-sm-offset-2">
 				<div class="countdown" data-date="{{ Config::get('igda.event_date') }}"></div>
 			</div>
 		</div>
@@ -148,10 +148,10 @@
 	<div class="igda">
 		<div class="container">
 			<div class="row">
-				<div class="col-md-4">
+				<div class="col-sm-4">
 					<img src="{{ asset('img/igda_cr_logo.png') }}" class="img-responsive" alt="@lang('global.igda_cr')">
 				</div>
-				<div class="col-md-8">
+				<div class="col-sm-8">
 					<h1 class="transformed">@lang('gamefest.igda')</h1>
 					<p>@lang('gamefest.igda_message')</p>
 				</div>
